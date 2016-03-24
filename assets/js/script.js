0c993f30207299d4a771966fa276df2bc6e12da4
dargaCode <darga.code@gmail.com>
2016-03-24 00:57:14 -0700

    Toggle completed class on any li when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,8 @@ var itemInput = $("#text-input");
 var list = $("#todo-items");
 var items = $("#todo-items li");
 var deleteButtons = $("#todo-items span");
+
+items.on("click", function() {
+  $(this).toggleClass("completed");
+});
+
