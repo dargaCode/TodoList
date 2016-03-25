9364522b84e619652247538df6fb98c6f4fcde03
dargaCode <darga.code@gmail.com>
2016-03-25 01:47:34 -0700

    Remove variables that are no longer used
    - These previously had events applied to them. Now the
    events are applied to the ul instead, and restricted by
    selectors.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,6 @@ var ENTER_KEY_ID = 13;
 
 var itemInput = $("#text-input");
 var list = $("#todo-items");
-var items = $("#todo-items li");
-var deleteButtons = $("#todo-items span");
 
 list.on("click", "li", function() {
   $(this).toggleClass("completed");
