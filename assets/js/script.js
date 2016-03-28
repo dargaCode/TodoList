cd8af2dce98d491185b5652341fe2aeb8cc1cacd
dargaCode <darga.code@gmail.com>
2016-03-28 00:23:34 -0700

    Call addEditSubmitEvent() from addEvents()

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@ var list = $("#tasks");
 
 function addEvents() {
   addEditToggleEvent();
+  addEditSubmitEvent();
   addItemCreateEvent();
   addItemToggleCompletedEvent();
   addItemDestroyEvent();
